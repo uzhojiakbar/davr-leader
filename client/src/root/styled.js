9470a854a1a7d40004b7257344caabb8e5554b31
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-06-30 04:18:11 +0500

    Media Bug fixed

--- client/src/root/styled.js
+++ client/src/root/styled.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
-const RootContainer = styled.div``;
+const RootContainer = styled.div`
+  width: 100vw;
+  min-width: 350px;
+  max-width: 1920px;
+  margin: 0 auto;
+`;
 
 export { RootContainer };
